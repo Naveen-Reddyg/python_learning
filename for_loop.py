b07f0b3c3cacf58f24bfe787a7582b5356c75311
Naveen <naveenreddy3793@gmail.com>
2020-03-06 00:00:38 -0700

    added if else condition

--- for_loop.py
+++ for_loop.py
@@ -1,5 +1,9 @@
 x = int(input("Enter a number for range"))
 for i in range(x):
     print(i)
+    if i % 2 == 0:
+        print("Even Number")
+    else:
+        print("Odd Number")
 
 print("end of the line")
