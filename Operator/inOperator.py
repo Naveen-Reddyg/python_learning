617d06488c6d1d04b7eee2b9964929d2da240b78
Naveen-sudo <naveenreddy3793@gmail.com>
2020-03-25 19:24:49 -0700

    python operator example

---
+++ Operator/inOperator.py
@@ -0,0 +1,18 @@
+#Operator examples
+#identity operators
+#is and is not(which shows the identity of operators)
+n = 123
+b = 123
+v = 122
+print( n is b)  # gives true because n and b values same so address also same
+print(n is v)   # false
+print(n is not v) 
+
+#membership operators
+#in and not in 
+s = "Hello World"
+print("Hello" in s)
+print("naveen" not in s)
+lis = [1,2,3]
+print(1 in lis)
+
