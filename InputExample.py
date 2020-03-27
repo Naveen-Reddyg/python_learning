9159221aee20ddb3f624b2a109ae8d21b8788d44
Naveen-sudo <naveenreddy3793@gmail.com>
2020-03-26 17:53:40 -0700

    Input examples summary

---
+++ InputExample.py
@@ -0,0 +1,14 @@
+#Input part
+# 1) input
+# 2) typecast to required type int(),float(),bool()
+# 3) eval()
+
+
+# commandline argumnets
+# argv and sys module
+
+from sys import argv
+print(int(argv[1])+int(argv[2]))
+
+
+
