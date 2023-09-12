00e5a2cd98874fce6388977482fbe0a9e005d95e
naveeng <naveenreddy3793@gmail.com>
2023-09-11 23:41:22 -0400

    Day 1 of #100DaysOfCode

---
+++ 100Days_of_Python/Day1/main.py
@@ -0,0 +1,17 @@
+# To print the data we can use the print function
+print("hello world!")
+
+
+# To make manipulate the code of the string we can use the "+" to add different strings together
+print("Hello " + "world!")
+print("Hello"+" "+"world!")
+
+
+# To take the input from the user we use the input function
+print("Hello" + input("what is your name?"))
+
+# we can use the variables to store the data and use these variables for our reference
+name = input("what is your name?")
+print("Hello " + name)
+
+
